4e549f4b4b28e685d3dca31cb23c53853cc0b92f
Dstefanescu2002 <dstefanescu1609@gmail.com>
2022-02-19 16:10:11 -0500

    Changed to work with generated coordinate file (NOT TESTED)

--- jetson/nav/search/spiralOutSearch.cpp
+++ jetson/nav/search/spiralOutSearch.cpp
@@ -11,33 +11,40 @@ SpiralOut::~SpiralOut() {}
 // of the search.
 void SpiralOut::initializeSearch( Rover* rover, const rapidjson::Document& roverConfig, const double visionDistance )
 {
-    mSearchPoints.clear();
-
-    mSearchPointMultipliers.clear();
-    mSearchPointMultipliers.push_back( pair<short, short> (  0,  1 ) );
-    mSearchPointMultipliers.push_back( pair<short, short> ( -1,  1 ) );
-    mSearchPointMultipliers.push_back( pair<short, short> ( -1, -1 ) );
-    mSearchPointMultipliers.push_back( pair<short, short> (  1, -1 ) );
-
-    while( mSearchPointMultipliers[ 0 ].second * visionDistance < roverConfig[ "search" ][ "bailThresh" ].GetDouble() ) {
-        for( auto& mSearchPointMultiplier : mSearchPointMultipliers )
-        {
-            Odometry nextSearchPoint = rover->roverStatus().path().front().odom;
-            double totalLatitudeMinutes = nextSearchPoint.latitude_min +
-                ( mSearchPointMultiplier.first * visionDistance  * LAT_METER_IN_MINUTES );
-            double totalLongitudeMinutes = nextSearchPoint.longitude_min +
-                ( mSearchPointMultiplier.second * visionDistance * rover->longMeterInMinutes() );
-            nextSearchPoint.latitude_deg += totalLatitudeMinutes / 60;
-            nextSearchPoint.latitude_min = ( totalLatitudeMinutes - ( ( (int) totalLatitudeMinutes ) / 60 ) * 60 );
-            nextSearchPoint.longitude_deg += totalLongitudeMinutes / 60;
-            nextSearchPoint.longitude_min = ( totalLongitudeMinutes - ( ( (int) totalLongitudeMinutes) / 60 ) * 60 );
-
-            mSearchPoints.push_back( nextSearchPoint );
-
-            mSearchPointMultiplier.first < 0 ? --mSearchPointMultiplier.first : ++mSearchPointMultiplier.first;
-            mSearchPointMultiplier.second < 0 ? --mSearchPointMultiplier.second : ++mSearchPointMultiplier.second;
-
-        }
+    ifstream coordinate_file ("search_points.txt");
+    short rho, phi;
+    while (coordinate_file >> rho >> phi) {
+        Odometry nextSearchPoint = createOdom(rover->roverStatus().path().front().odom, phi, rho, rover);
+        mSearchPoints.push_back( nextSearchPoint );
     }
-    insertIntermediatePoints();
+
+    // mSearchPoints.clear();
+
+    // mSearchPointMultipliers.clear();
+    // mSearchPointMultipliers.push_back( pair<short, short> (  0,  1 ) );
+    // mSearchPointMultipliers.push_back( pair<short, short> ( -1,  1 ) );
+    // mSearchPointMultipliers.push_back( pair<short, short> ( -1, -1 ) );
+    // mSearchPointMultipliers.push_back( pair<short, short> (  1, -1 ) );
+
+    // while( mSearchPointMultipliers[ 0 ].second * visionDistance < roverConfig[ "search" ][ "bailThresh" ].GetDouble() ) {
+    //     for( auto& mSearchPointMultiplier : mSearchPointMultipliers )
+    //     {
+    //         Odometry nextSearchPoint = rover->roverStatus().path().front().odom;
+    //         double totalLatitudeMinutes = nextSearchPoint.latitude_min +
+    //             ( mSearchPointMultiplier.first * visionDistance  * LAT_METER_IN_MINUTES );
+    //         double totalLongitudeMinutes = nextSearchPoint.longitude_min +
+    //             ( mSearchPointMultiplier.second * visionDistance * rover->longMeterInMinutes() );
+    //         nextSearchPoint.latitude_deg += totalLatitudeMinutes / 60;
+    //         nextSearchPoint.latitude_min = ( totalLatitudeMinutes - ( ( (int) totalLatitudeMinutes ) / 60 ) * 60 );
+    //         nextSearchPoint.longitude_deg += totalLongitudeMinutes / 60;
+    //         nextSearchPoint.longitude_min = ( totalLongitudeMinutes - ( ( (int) totalLongitudeMinutes) / 60 ) * 60 );
+
+    //         mSearchPoints.push_back( nextSearchPoint );
+
+    //         mSearchPointMultiplier.first < 0 ? --mSearchPointMultiplier.first : ++mSearchPointMultiplier.first;
+    //         mSearchPointMultiplier.second < 0 ? --mSearchPointMultiplier.second : ++mSearchPointMultiplier.second;
+
+    //     }
+    // }
+    // insertIntermediatePoints();
 } // initializeSearch()
